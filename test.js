3c216c720cab29ae1f2b605e5a477b0ca932a4fb
Hans Kristian Flaatten <hans.kristian.flaatten@dnt.no>
2016-05-31 20:18:40 +0200

    chore(use): use arrow callback functions

--- test.js
+++ test.js
@@ -3,15 +3,15 @@
 const assert = require('assert');
 const HttpError = require('.');
 
-describe('HttpError', function describe() {
-  it('returns new HttpError', function it() {
+describe('HttpError', () => {
+  it('returns new HttpError', () => {
     const httpError = new HttpError('Not Found', 404);
 
     assert.equal(httpError.message, 'Not Found');
     assert.equal(httpError.code, 404);
   });
 
-  it('returns json representation', function it() {
+  it('returns json representation', () => {
     const httpError = new HttpError('Not Found', 404);
 
     assert.deepEqual(httpError.toJSON(), {
@@ -20,13 +20,13 @@ describe('HttpError', function describe() {
     });
   });
 
-  it('returns string representation', function it() {
+  it('returns string representation', () => {
     const httpError = new HttpError('Not Found', 404);
 
     assert.equal(httpError.toString(), '404: Not Found');
   });
 
-  it('wraps existing error', function it() {
+  it('wraps existing error', () => {
     const error = new Error('Original Error');
     const httpError = new HttpError('Not Found', 404, error);
 
